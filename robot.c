3564cf80a727645eb4ed2bc021ff09bfd964c472
Huesteus <huey17@up.edu>
2014-10-22 23:33:48 +0000

    Gave the robot a facelift. Added myself to the authorlist.

--- robot.c
+++ robot.c
@@ -4,7 +4,7 @@
  * A file that contains the supporting functions for main() in the
  * ~/robots example source for Software Engineering.
  *
- * @author Tanya L. Crenshaw
+ * @author Tanya L. Crenshaw and Nick Huey
  * @since August 2013
  *
  */
@@ -45,8 +45,8 @@ void robPrintMessage(int num)
  */
 void robPrintAscii(void)
 {
-  printf("    i_i    \n"
-	 "   [@_@]   \n"
+  printf("    i_i  \n"
+	 "   [-_-]   \n"
 	 "  /|___|\\  \n"
          "   d   b   "); 
 
